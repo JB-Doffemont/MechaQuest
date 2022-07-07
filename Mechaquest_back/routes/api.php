8a2d3650c2ffcb2a3a1d47e8d01c83d58b683b4f
SpyX <gxcspyx@gmail.com>
2022-07-07 12:25:37 +0200

    destroy exclue des ressources

--- Mechaquest_back/routes/api.php
+++ Mechaquest_back/routes/api.php
@@ -28,27 +28,33 @@ Route::post('/login', [AuthController::class, 'login']);
 
 // Routes ressources
 Route::group(['middleware' => 'auth:sanctum'], function () {
-    Route::resources([
-        'users' => UserController::class,
-        'robots' => RobotController::class,
-        'areas' => AreaController::class,
-        'progression' => ProgressionController::class,
-        'types' => TypeController::class,
-        'positions' => PositionController::class,
-        'friends' => FriendController::class,
-    ]);
+    Route::resources(
+        [
+            'users' => UserController::class,
+            'robots' => RobotController::class,
+            'areas' => AreaController::class,
+            'progression' => ProgressionController::class,
+            'types' => TypeController::class,
+            'positions' => PositionController::class,
+            'friends' => FriendController::class,
+        ],
+        ['except' => ['destroy']]
+    );
+
+    Route::group(['middleware' => 'isAdmin'], function () {
+
+
+        // Route pour récupérer les données supprimées
+        Route::post('users/{user}/restore', [UserController::class, 'restore'])->name('users.restore');
+        Route::post('areas/{area}/restore', [AreaController::class, 'restore'])->name('areas.restore');
+        Route::post('robots/{robot}/restore', [RobotController::class, 'restore'])->name('robots.restore');
+        Route::post('progression/{progression}/restore', [ProgressionController::class, 'restore'])->name('progression.restore');
+        Route::post('types/{type}/restore', [TypeController::class, 'restore'])->name('types.restore');
+        Route::post('positions/{area}/{position}/restore', [PositionController::class, 'restore'])->name('positions.restore');
+
+
+        Route::put('positions/{area}/{position}', [PositionController::class, 'update'])->name('positions.update');
+        Route::delete('positions/{area}/{position}', [PositionController::class, 'destroy'])->name('positions.destroy');
+        Route::delete('friends/{user1}/{user2}', [FriendController::class, 'deleteFriend'])->name('friends.deleteFriend');
+    });
 });
-
-
-// Route pour récupérer les données supprimées
-Route::post('users/{user}/restore', [UserController::class, 'restore'])->name('users.restore')->middleware('auth:sanctum');
-Route::post('areas/{area}/restore', [AreaController::class, 'restore'])->name('areas.restore')->middleware('auth:sanctum');
-Route::post('robots/{robot}/restore', [RobotController::class, 'restore'])->name('robots.restore')->middleware('auth:sanctum');
-Route::post('progression/{progression}/restore', [ProgressionController::class, 'restore'])->name('progression.restore')->middleware('auth:sanctum');
-Route::post('types/{type}/restore', [TypeController::class, 'restore'])->name('types.restore')->middleware('auth:sanctum');
-Route::post('positions/{area}/{position}/restore', [PositionController::class, 'restore'])->name('positions.restore')->middleware('auth:sanctum');
-
-
-Route::put('positions/{area}/{position}', [PositionController::class, 'update'])->name('positions.update')->middleware('auth:sanctum');
-Route::delete('positions/{area}/{position}', [PositionController::class, 'destroy'])->name('positions.destroy')->middleware('auth:sanctum');
-Route::delete('friends/{user1}/{user2}', [FriendController::class, 'deleteFriend'])->name('friends.deleteFriend')->middleware('auth:sanctum');
--- Mechaquest_front/App.js
+++ Mechaquest_front/App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import logo from "./assets/logo.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>MECHA QUEST</Text>
-      <StatusBar style="auto" />
+      <Image source={logo} style={styles.logo} />
+
+      {/* <Text style={styles.title}>MECHA QUEST</Text> */}
+
+      <TouchableOpacity onPress={() => alert("$ boule")} style={styles.bouton}>
+        <Text style={styles.bouton_text}>Découvrir</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +19,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "black",
     alignItems: "center",
     justifyContent: "center",
+  },
+  logo: {
+    width: 605,
+    height: 359,
+    marginBottom: 10,
+  },
+  title: {
+    color: "#888",
+    fontSize: 58,
+    marginHorizontal: 15,
+  },
+  bouton: {
+    backgroundColor: "#49ada5",
+    padding: 20,
+    borderRadius: 5,
+    transition: 0.5,
+    color: "#41403E",
+    // font-size:2rem;
+    // letter-spacing:1px;
+    // outline:none;
+    // box-shadow: 20px 38px 34px -26px hsla(0,0%,0%,.2);
+    // border-radius: 255px 15px 225px 15px/15px 225px 15px 255px;
+  },
+  bouton_text: {
+    fontSize: 20,
+    color: "#fff",
   },
 });

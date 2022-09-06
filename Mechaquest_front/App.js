f1e86e7def0abf29a6891d7bb08a251ea81ce5d8
SpyX <gxcspyx@gmail.com>
2022-09-06 10:19:02 +0200

    react navigation home to connexion

--- Mechaquest_front/App.js
+++ Mechaquest_front/App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import SignUp from "./src/components/SignUpForm";
+import LogInForm from "./src/components/LogInForm";
 import StarterScreen from "./src/components/StarterScreen";
 
 const Stack = createNativeStackNavigator();
@@ -14,6 +15,11 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="StarterScreen" component={StarterScreen} />
+        <Stack.Screen
+          name="LogIn"
+          component={LogInForm}
+          options={{ title: "Log-in" }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Mechaquest_front/src/components/StarterScreen.jsx
+++ Mechaquest_front/src/components/StarterScreen.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import logo from "../assets/logo.png";
 import styles from "../style/StarterScreenStyle"
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Image, Text, TouchableOpacity, View } from "react-native";
 
-export default function StarterScreen() {
+export default function StarterScreen({navigation}) {
     return(
        <View style={styles.container}>
         <Image source={logo} style={styles.logo} />
-        <TouchableOpacity onPress={() => alert("$ boule")} style={styles.button}>
-            <Text style={styles.button_text}>Découvrir</Text>
+        <TouchableOpacity         onPress={() => navigation.navigate('LogIn')}
+ style={styles.button}>
+            <Text style={styles.button_text}>S'inscrire</Text>
+        </TouchableOpacity>
+        <TouchableOpacity         onPress={() => navigation.navigate('LogIn')}
+ style={styles.button}>
+            <Text style={styles.button_text}>Connexion</Text>
         </TouchableOpacity>
        </View>
     );

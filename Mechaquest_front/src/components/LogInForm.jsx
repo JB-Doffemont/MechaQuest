2994ab014784abdb19edc6d1db788c53be4da47f
SpyX <gxcspyx@gmail.com>
2022-09-06 14:11:52 +0200

    login form without validation

--- Mechaquest_front/src/components/LogInForm.jsx
+++ Mechaquest_front/src/components/LogInForm.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { useState } from "react";
+import { View } from "react-native";
 import InputWithLabel from "./usable/InputWithLabel";
 
 export default function SignUp() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
     return(
-        <InputWithLabel/>
+        <View>
+            <InputWithLabel 
+                label="Email"
+                value={email}
+                onChangeText={setEmail}
+                placeholder="Entrez votre e-mail"
+                />
+            <InputWithLabel 
+                label="Mot de passe"
+                value={password}
+                onChangeText={setPassword}
+                placeholder="Entrez votre mot de passe"
+                secureTextEntry
+                />
+       
+        </View>
     );
 }
 

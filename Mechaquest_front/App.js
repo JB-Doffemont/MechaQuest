829bc7b6d55026d772b42e940c4596f4382c4c42
JB <jeanbernard_doffemont@yahoo.fr>
2022-09-06 10:01:29 +0200

    navigation

--- Mechaquest_front/App.js
+++ Mechaquest_front/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { View, Text, TextInput } from "react-native";
 import styles from "./src/style/AppStyle";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import SignUp from "./src/components/SignUpForm";
 import StarterScreen from "./src/components/StarterScreen";
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StarterScreen />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="StarterScreen" component={StarterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }

9d84ce0b96fbdec739dcbf6ecd7205b983f3d7c0
JB <jeanbernard_doffemont@yahoo.fr>
2022-09-06 09:19:17 +0200

    input component

---
+++ Mechaquest_front/src/style/InputStyle.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  inputsView: {
+    padding: 18,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  label: {
+    color: "white",
+    padding: 8,
+    fontSize: 18,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});

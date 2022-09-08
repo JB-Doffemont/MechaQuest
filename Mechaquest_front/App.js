206caf85cf2e2f75d47d622238eb3b212fcdf282
JB <jeanbernard_doffemont@yahoo.fr>
2022-09-08 12:08:30 +0200

    IntroScreen component

--- Mechaquest_front/App.js
+++ Mechaquest_front/App.js
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SignUpForm from "./src/components/SignUpForm";
 import LogInForm from "./src/components/LogInForm";
 import StarterScreen from "./src/components/StarterScreen";
+import IntroScreen from "./src/components/IntroScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -19,6 +20,7 @@ export default function App() {
           options={{ title: "Log-in" }}
         />
         <Stack.Screen name="SignUpForm" component={SignUpForm} />
+        <Stack.Screen name="IntroScreen" component={IntroScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ Mechaquest_front/src/components/IntroScreen.jsx
@@ -0,0 +1,9 @@
+import { View, ScrollView } from "react-native";
+
+export default function IntroScreen() {
+    return (
+        <View>
+            
+        </View>
+    );
+}

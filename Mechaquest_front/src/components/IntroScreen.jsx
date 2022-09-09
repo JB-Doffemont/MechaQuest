7c0d92a9ed4e3ed644753c305f69f0b83a5d5c46
JB <jeanbernard_doffemont@yahoo.fr>
2022-09-09 09:51:33 +0200

    IntroScreen component

--- Mechaquest_front/src/components/IntroScreen.jsx
+++ Mechaquest_front/src/components/IntroScreen.jsx
@@ -1,9 +1,21 @@
-import { View, ScrollView } from "react-native";
+import { View, ScrollView, Text } from "react-native";
+import styles from "../style/IntroScreen";
 
 export default function IntroScreen() {
     return (
-        <View>
-            
+        <View style={styles.container}>
+            <Text style={{color:"white"}}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+                obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+                nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+                tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+                quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+                sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+                recusandae alias error harum maxime adipisci amet laborum.
+            </Text>
         </View>
     );
 }

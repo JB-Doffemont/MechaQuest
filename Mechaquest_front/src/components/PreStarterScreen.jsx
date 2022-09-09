a13e068e13577139fc6973fe26bfda2a2db8ba1d
SpyX <gxcspyx@gmail.com>
2022-09-09 16:24:34 +0200

    commentaire pour la prochaine fois

--- Mechaquest_front/src/components/PreStarterScreen.jsx
+++ Mechaquest_front/src/components/PreStarterScreen.jsx
@@ -2,6 +2,8 @@
 - Si présence du token mais pas de robot selectionné, doit renvoyer sur le tuto puis selection du robot (if getItem !== null && 1st_co == 0)
 - Si présence du token et d'un robot selectionné, doit renvoyer sur la home page du joueur (if getItem !== null && 1st_co == 1)
 - Si pas de présence du token, doit renvoyer sur le "Starter Screen Unco" pour permettre de s'enregistrer ou de se connecter (if getItem == null)
+
+Il va falloir faire une requete en back qui compare notre "access_token" à l'auth token qui est dans la table "personnal access token" afin de récupérer l'email.
  */
 
 import React from "react";

da94f0dea8a86bc7a84853a74775e4c33e801980
SpyX <gxcspyx@gmail.com>
2022-12-06 10:12:52 +0100

    ipconfig on my branch..

--- Mechaquest_front/IpConfig.js
+++ Mechaquest_front/IpConfig.js
@@ -1,6 +1,6 @@
 // Ce fichier config est utilisé pour changer l'adresse IP des requêtes en BDD (JB ou Lucas)
 
-const ipConfig = "http://192.168.43.192:8000";
+const ipConfig = "http://172.20.10.7:8000";
 // http://172.20.10.7:8000 (Iphone Lucas)
 // http://192.168.43.192:8000 (Android JB)
 
